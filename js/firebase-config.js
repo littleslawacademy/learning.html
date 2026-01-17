391d0f53355b463cdd0383f51042fc014f0a568e
vasanthshanmugam <vasanthtce@gmail.com>
2026-01-17 19:41:34 +0000

    fix login

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,10 +1,2 @@
-// This file will be updated by GitHub Actions
-const firebaseConfig = {
-    apiKey: "GH_INJECT_API_KEY",
-    authDomain: "GH_INJECT_AUTH_DOMAIN",
-    projectId: "GH_INJECT_PROJECT_ID",
-    storageBucket: "GH_INJECT_STORAGE_BUCKET",
-    messagingSenderId: "GH_INJECT_SENDER_ID",
-    appId: "GH_INJECT_APP_ID",
-    measurementId: "GH_INJECT_MEASUREMENT_ID"
-};
+// This file is intentionally left blank for local development.
+// The firebaseConfig is defined in js/main.js
