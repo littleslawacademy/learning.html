4b3f39bb8d87830bcc50f84c13b34235226767c2
vasanthshanmugam <32473491+vasanthshanmugam@users.noreply.github.com>
2026-01-17 19:31:23 +0000

    Deploying to gh-pages from @ littleslawacademy/learning.html@96e3d41728f69c5ec6b290f202ed280281a58fd5 🚀

---
+++ js/firebase-config.js
@@ -0,0 +1,10 @@
+// This file will be updated by GitHub Actions
+const firebaseConfig = {
+    apiKey: "AIzaSyAIM7NkLymWvPOFfGJlUI3ZyGKIgAhVFuI",
+    authDomain: "littleslawacademy-f45e7.firebaseapp.com",
+    projectId: "littleslawacademy-f45e7",
+    storageBucket: "littleslawacademy-f45e7.firebasestorage.app",
+    messagingSenderId: "393189119362",
+    appId: "1:393189119362:web:659d157ab482c4a660ab9b",
+    measurementId: "G-S40XF238WM"
+};
